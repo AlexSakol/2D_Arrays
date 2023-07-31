b1f8dee03ce1e624329858166d48305b3c6f5ddb
Alex <sakalinski95@gmail.com>
2023-07-31 21:32:44 +0300

    Добавьте файлы проекта.

---
+++ 2D_Arrays/2D_Arrays.cpp
@@ -0,0 +1,92 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+void massCreate(double** mas, int* n, int* m) {
+	for (int i = 0; i < *n; i++) {
+		printf("Enter the elements of %d string\n", i);
+		for (int j = 0; j < *m; j++) {
+			printf("[%d][%d]= ", i, j);
+			scanf_s("%lf", *(mas + i) + j);
+		}
+		printf("\n");
+	}
+
+}
+
+void massPrint(double** mas, int* n, int* m) {
+	printf("Created array\n");
+
+	for (int i = 0; i < *n; i++) {
+		for (int j = 0; j < *m; j++) {
+			printf("[%d][%d]= %.4lf\t", i, j, *(*(mas + i) + j));
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void task1(double** mas, int n, int* m) {// Divide diagonal elements by max
+	int i, j;
+	double div1, div2;
+	double max = **mas;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < *m; j++) {
+			if (max < mas[i][j]) {
+				max = mas[i][j];
+			}
+		}
+	}
+	printf("Max=%.4lf\n", max);
+	printf("Diagonals divided by max\n");
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < *m; j++) {
+			if (i == j) {
+				div1 = mas[i][j] / max;
+				printf("%.4lf ", div1);
+			}
+		}
+	}
+	for (int i = (n - 1); i >= 0; --i)
+	{
+		div2 = mas[i][n - i - 1] / max;
+		printf("%.4lf ", div2);
+	}
+}
+
+
+
+void main() {
+
+	int m, n;
+	double** mas;
+
+	printf("Enter the number of rows: ");
+	scanf_s("%d", &n);
+	printf("\nEnter the number of columns: ");
+	scanf_s("%d", &m);
+
+	if (!(mas = new double* [n])) {
+		printf("Out of memory\n");
+		exit(0);
+	}
+
+	for (int i = 0; i < n; i++)
+		if (!(*(mas + i) = new double[m])) {
+			printf("Out of memory\n");
+			mas = NULL;
+			exit(0);
+		}
+	if (n == m) {
+		massCreate(mas, &n, &m);
+		massPrint(mas, &n, &m);
+		task1(mas, n, &m);
+	}
+	else { printf("Array is not square"); }
+
+	for (int i = 0; i < n; i++)
+		delete[](*(mas + i));
+	delete[]mas;
+
+	_getch();
+}
---
+++ 2D_Arrays_2/2D_Arrays_2.cpp
@@ -0,0 +1,59 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+void task1(int* mas, int n, int m) { // average of columns with negative elements
+	int count = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			double average = 0, sum = 0;
+			if (*(mas + i * m + j) < 0) {
+				count++;
+				printf("Column with negative elements\n");
+				for (int i = 0; i < n; i++) {
+					printf("%5d\n", *(mas + i * m + j));
+					sum += *(mas + i * m + j);
+					average = sum / n;
+				}
+				printf("Average %.4lf\n", average);
+			}
+		}
+	}
+	if (count == 0) { printf("Columns with negative elements do not exist\n"); }
+
+}
+
+
+void main() {
+
+	int* mass, i, j, n, m;
+
+	printf("Strings: ");
+	scanf_s("%d", &n);
+	printf("Columns: ");
+	scanf_s("%d", &m);
+
+	mass = (int*)malloc(n * m * sizeof(int));
+
+	if (!mass) { printf("Out of memory\n"); }
+	else {
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < m; j++) {
+				printf("a[%d][%d] = ", i, j);
+				scanf_s("%d", (mass + i * m + j));
+			}
+		}
+		printf("\n");
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < m; j++) {
+				printf("%5d ", *(mass + i * m + j));
+			}
+			printf("\n");
+		}
+		task1(mass, n, m);
+	}
+	_getch();
+	free(mass);
+}
+
